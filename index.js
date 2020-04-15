74e7a9751c832ea3b177f3fdada5044dbcd6fd80
Roberto Serrano <robert.serrano77@gmail.com>
2020-04-14 21:24:12 -0500

    Correcciones deployment

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extend: true }));
 
 //Puerto de la app
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 //Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -25,6 +25,6 @@ app.use('/api/tareas', require('./routes/tareas'));
 
 
 //Arrancar la app
-app.listen(port, '0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`El servidor está funcionando en el puerto ${PORT}`);
 });
